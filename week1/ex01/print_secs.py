1a72add6d1b55fdcaa514b015ea226ee5b6e4c06
keiji11 <lucente.luca@gmail.com>
2022-05-19 00:34:46 +0200

    ex01

---
+++ week1/ex01/print_secs.py
@@ -0,0 +1,21 @@
+"""
+Convert all in seconds
+"""
+import sys
+
+print("Usage: ./print_secs.py <hour> ?<minute> ?<second>")
+try:
+  if (len(sys.argv) > 4) or (len(sys.argv) < 2):
+    raise ValueError("Invalid number of arguments")
+  else:
+    if len(sys.argv) >= 2:
+      ret = int(sys.argv[1]) * 60 * 60
+    if len(sys.argv) >= 3:
+      ret += int(sys.argv[2]) * 60
+    if len(sys.argv) == 4:
+      ret += int(sys.argv[3])
+    print("Total number of seconds:", ret)
+except ValueError as e:
+  print(e)
+  print("Usage: ./print_secs.py <hour> ?<minute> ?<second>")
+  sys.exit(1)
