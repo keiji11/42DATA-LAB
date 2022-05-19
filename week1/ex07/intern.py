b50b9400768d2e5d09e390a14733d30932a75b22
keiji11 <lucente.luca@gmail.com>
2022-05-19 18:02:19 +0200

    ex07 ended...maybe

--- week1/ex07/intern.py
+++ week1/ex07/intern.py
@@ -1,7 +1,26 @@
-class Intern:
+class Coffee:
+  def __str__(self):
+    return "This is the worst coffee you ever tasted."
+
+class Intern(Coffee):
   def __init__(self, name="My name? I’m nobody, an intern, I have no name."):
     self.name = name
+  def __str__(self):
+    return self.name
 
-obj = Intern()
-obj.name = "Guido"
-print(obj.name)
+def make_coffee(intern):
+  return Coffee()
+def work(e):
+  raise Exception("I’m just an intern, I can’t do that...")
+
+intern1 = Intern()
+intern2 = Intern("Mark")
+try:
+  print(intern1.__str__())
+  print(intern2.__str__())
+  print(intern2.name,", may I have a coffee please?")
+  print(make_coffee(intern1))
+  print("May I have some work here please?")
+  print(work(intern2))
+except Exception as e:
+  print(e)
